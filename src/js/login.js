45cd200825618ac227393a37415baaed26b8e749
酒井美幸 <ciel.bleu.d@gmail.com>
2023-12-31 14:48:50 +0900

    ヘッダー・firstview・gozonji

---
+++ src/js/login.js
@@ -0,0 +1,9 @@
+document.querySelector('.signin-button').addEventListener('click', function() {
+
+    console.log("hello");
+
+    var idValue = document.getElementById('id').value;
+    var passwordValue = document.getElementById('password').value;
+    console.log('ID:', idValue, 'Password:', passwordValue);
+
+});
